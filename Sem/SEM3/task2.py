d1691ea45714f5e8232e0f1999e617afa4e88d4f
AlexKr <kaf-2012@mail.ru>
2023-04-03 22:32:33 +0300

    task2 v2.0

--- Sem/SEM3/task2.py
+++ Sem/SEM3/task2.py
@@ -1,21 +1,21 @@
-# dict = {'Verer': 0, 'Vsd': 1, 'VI': 'S001', 'VI': 'S005', 'VII': ' S005 ', ' V ': ' S005', ' VII ': ' S005 ', 'V': ' S009 ', 'IV': 'S005'}
+dict = {'Verer': 0, 'Vsd': 1, 'VI': 'S001', 'VI': 'S005', 'VII': ' S005 ', ' V ': ' S005', ' VII ': ' S005 ', 'V': ' S009 ', 'IV': 'S005'}
 
-# print(dict)
-# my_set = set(dict.values()) 
-# print(my_set)
+print(dict)
+my_set = set(dict.values()) 
+print(my_set)
 
 # Вариант 2
-a = {'V': "S001", 'V': "S002", 'VI': "S001", 'VI': "S005",
-     'VI': "S005", 'VII': "  S005 ", " V ": " S009 ", " VIII ": " S007 ", "IV" : "S002"}
-b = list()
-for el in a:
-    b.append(a[el])
-print(set(b))
-a = [4,3,2,6,5,9,0]
-counter = 0
-temp = a[0]
-for el in a:
-    if el > temp:
-        counter += 1
-    temp = el
-print(counter)
+# a = {'V': "S001", 'V': "S002", 'VI': "S001", 'VI': "S005",
+#      'VI': "S005", 'VII': "  S005 ", " V ": " S009 ", " VIII ": " S007 ", "IV" : "S002"}
+# b = list()
+# for el in a:
+#     b.append(a[el])
+# print(set(b))
+# a = [4,3,2,6,5,9,0]
+# counter = 0
+# temp = a[0]
+# for el in a:
+#     if el > temp:
+#         counter += 1
+#     temp = el
+# print(counter)
